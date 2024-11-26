d637dd3b0658c66bae5a228c50766776185bc6c9
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 18:39:00 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -1,3 +1,12 @@
-!pip install streamlit 
+import pandas as pd
+
+# Substitua pelo caminho correto do arquivo
+file_path = "Demandas2024.xlsx"
+try:
+    data = pd.read_excel(file_path)
+    print(data.head())
+except Exception as e:
+    print("Erro ao carregar o arquivo Excel:", e)
+ 
 
 
