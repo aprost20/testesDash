6587b28877f31c5d3478d50553193c418810768f
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 16:23:19 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import requests as req
 
-st.title('Demandas TCU recebidas pelo MPO em 2024')
-df = pd.read_csv('/content/Demandas2024.csv')
+st.title('Demandas TCU 2024 recebidas pelo MPO')
+uploaded_file = st.file_uploader("/content/Demandas2024.xlsx", type=["xlsx"])
+
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file, engine='openpyxl')
+
+st.subheader('Visão geral dos dados')
+st.write(df.head())
 
