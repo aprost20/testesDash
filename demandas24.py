dcb26452d0727275400247a0db7587a5cea74b80
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 16:01:14 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -6,6 +6,4 @@ df = pd.read_excel('/content/Demandas2024.xlsx')
 
 demandas = df['Estado do SISCOD'].unique()
 demandasFiltro = st.selectbox('Selecione o status da demanda', demandas)
-dadosFiltrados = df[df['Estado do SISCOD'] == demandasFiltro]
-if st.checkbox('Mostrar tabela'):
-  st.write(dadosFiltrados)
+
