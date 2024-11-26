3042c5fe47be3bee3f7fa97e83e0b678878b5b4a
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 17:49:55 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -4,5 +4,11 @@ import pandas as pd
 st.title('Localização das comunidades quilombolas (2022)')
 df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
-
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
 
