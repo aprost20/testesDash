1a8299fdb43a864ffc5b7f5dac1e23f04e3f88ce
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 18:26:11 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = "Demandas2024.xlsx"  
+file_path = "/content/Demandas2024.xlsx"  
 data = pd.read_excel(file_path)
  
 
