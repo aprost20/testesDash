1c21278ccbd54fb1d3f083f90e6fdab782102c9b
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 17:51:40 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Localização das comunidades quilombolas (2022)')
-df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
-
-estados = df['NM_UF'].unique()
-estadoFiltro = st.selectbox(
-    'Qual estado selecionar?',
-     estados)
-dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
-if st.checkbox('Mostrar tabela'):
-  st.write(dadosFiltrados)
+st.title('Demandas TCU 2024 recebidas pelo MPO')
+df = pd.read_xlsx('Demandas2024.xlsx')
 
