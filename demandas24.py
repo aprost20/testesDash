af27dcc7d3b054d71acb9f492cc01b42fce6e26d
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 19:03:16 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.title('Demandas TCU')
-df = pd.read_excel('Demandas2024.xlsx')
+df = pd.read_excel('https://github.com/aprost20/testesDash/blob/main/Demandas2024.xlsx')
 
