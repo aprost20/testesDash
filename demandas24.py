7226119a19b599ede48358cad2f1e8d049fd267f
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 15:59:34 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
 
+st.title('Demandas TCU recebidas pelo MPO em 2024')
+df = pd.read_excel('/content/Demandas2024.xlsx')
+
+demandas = df['Estado do SISCOD'].unique()
+demandasFiltro = st.selectbox('Selecione o status da demanda', demandas)
+dadosFiltrados = df[df['Estado do SISCOD'] == demandasFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
