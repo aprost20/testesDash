a25f6701e48e29907895dce3586e287f24326c09
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 16:24:35 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -5,9 +5,5 @@ import requests as req
 st.title('Demandas TCU 2024 recebidas pelo MPO')
 uploaded_file = st.file_uploader("/content/Demandas2024.xlsx", type=["xlsx"])
 
-if uploaded_file is not None:
-    df = pd.read_excel(uploaded_file, engine='openpyxl')
 
-st.subheader('Visão geral dos dados')
-st.write(df.head())
 
