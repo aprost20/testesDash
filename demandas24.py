21de853fc95221b5338f0ef9b09161ddfde4c995
aprost20 <ana.rost@planejamento.gov.br>
2024-11-26 18:36:31 -0300

    Update demandas24.py

--- demandas24.py
+++ demandas24.py
@@ -1,8 +1,3 @@
-import streamlit as st
-import pandas as pd
+pip install streamlit pandas openpyxl
 
-uploaded_file = st.file_uploader("Carregue sua planilha Excel", type="xlsx")
-if uploaded_file is not None:
-    data = pd.read_excel(uploaded_file)
-    st.write(data.head())
 
